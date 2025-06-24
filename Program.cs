f14370bd725f64e29503d08c708a217c57e50699
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2025-06-24 20:36:01 +0200

    Update menu options

--- Program.cs
+++ Program.cs
@@ -13,14 +13,15 @@ namespace InventoryMgmtSystem
                 Console.WriteLine("1. Add Product");
                 Console.WriteLine("2. View Product");
                 Console.WriteLine("3. View All Products");
-                Console.WriteLine("4. Exit");
+                Console.WriteLine("4. Update Product");
+                Console.WriteLine("5. Exit");
 
                 string choice = Console.ReadLine();
 
                 switch (choice)
                 {
                     case "1":
-                        // AddProduct();
+                        AddProduct(inventoryList);
                         Console.WriteLine("Simulating adding product");
                         break;
                     case "2":
@@ -28,10 +29,14 @@ namespace InventoryMgmtSystem
                         Console.WriteLine("Simulating returning product");
                         break;
                     case "3":
-                        // ViewAllProducts();
+                        ViewAllProducts(inventoryList);
                         Console.WriteLine("Simulate returning all products in inventory management system");
                         break;
                     case "4":
+                        // UpdateProduct();
+                        Console.WriteLine("Simulating updating product");
+                        break;
+                    case "5":
                         Environment.Exit(0);
                         break;
                     default:
@@ -41,36 +46,42 @@ namespace InventoryMgmtSystem
             }
 
 
-            // string name = "";
-            // double price = 0;
-            // int quantity = 0;
 
-
-            // Console.WriteLine("Please enter a product name: ");
-            // name = Console.ReadLine();
-
-            // Console.WriteLine($"Please enter a product price for {name}: ");
-            // price = double.Parse(Console.ReadLine());
-
-            // Console.WriteLine($"Please enter a quantity: ");
-            // quantity = int.Parse(Console.ReadLine());
-
-            // AddProduct(inventoryList, name, price, quantity);
-
-            // Console.WriteLine($"Product Name is {name}");
-            // Console.WriteLine($"The price for {name} is: $ {price}");
-            // Console.WriteLine($"The current quantity for {name}'s is : {quantity}");
         }
 
-        static void AddProduct(Inventory inventory, string name, double price, int quantity)
+        static void AddProduct(Inventory inventoryList)
         {
+
+            string name = "";
+            double price = 0;
+            int quantity = 0;
+
+
+            Console.WriteLine("Please enter a product name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Please enter a product price for {name}: ");
+            price = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Please enter a quantity: ");
+            quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Product Name is {name}");
+            Console.WriteLine($"The price for {name} is: $ {price}");
+            Console.WriteLine($"The current quantity for {name}'s is : {quantity}");
+
             Product product = new Product
             {
                 Name = name,
                 Price = price,
                 Quantity = quantity
             };
-            inventory.AddProduct(product);
+            inventoryList.AddProduct(product);
+        }
+
+        static void ViewAllProducts(Inventory inventoryList )
+        {
+            
         }
     }
 }
