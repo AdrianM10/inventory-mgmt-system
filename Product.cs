9e2ceebd62bf78f9812b55a7ad0404492ca60447
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2025-06-24 20:35:43 +0200

    Add method to add product

--- Product.cs
+++ Product.cs
@@ -8,3 +8,22 @@ public class Product
     public int Quantity { get; set; }
     public double Price { get; set; }
 }
+
+public class Inventory
+{
+    public List<Product> products { get; set; } = new List<Product>();
+
+    // Simulate add product to inventory list
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    // Simulate viewing all products
+   
+
+
+    // Simulate updating product to inventory list
+
+    // Simulate removing product from inventory list
+}
