27a784dbec2630964c9d67fa5ac838affbdf48db
Adrian Mudzwiti <54492734+AdrianM10@users.noreply.github.com>
2025-06-24 20:21:30 +0200

    Prompt user for selection to add, view products or exit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using InventoryMgmtSystem;
+
+namespace InventoryMgmtSystem
+{
+    class Program
+    {
+        static Inventory inventoryList = new();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Inventory Management System");
+                Console.WriteLine("1. Add Product");
+                Console.WriteLine("2. View Product");
+                Console.WriteLine("3. View All Products");
+                Console.WriteLine("4. Exit");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        // AddProduct();
+                        Console.WriteLine("Simulating adding product");
+                        break;
+                    case "2":
+                        // ViewProduct();
+                        Console.WriteLine("Simulating returning product");
+                        break;
+                    case "3":
+                        // ViewAllProducts();
+                        Console.WriteLine("Simulate returning all products in inventory management system");
+                        break;
+                    case "4":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("You have selected an invalid option");
+                        break;
+                }
+            }
+
+
+            // string name = "";
+            // double price = 0;
+            // int quantity = 0;
+
+
+            // Console.WriteLine("Please enter a product name: ");
+            // name = Console.ReadLine();
+
+            // Console.WriteLine($"Please enter a product price for {name}: ");
+            // price = double.Parse(Console.ReadLine());
+
+            // Console.WriteLine($"Please enter a quantity: ");
+            // quantity = int.Parse(Console.ReadLine());
+
+            // AddProduct(inventoryList, name, price, quantity);
+
+            // Console.WriteLine($"Product Name is {name}");
+            // Console.WriteLine($"The price for {name} is: $ {price}");
+            // Console.WriteLine($"The current quantity for {name}'s is : {quantity}");
+        }
+
+        static void AddProduct(Inventory inventory, string name, double price, int quantity)
+        {
+            Product product = new Product
+            {
+                Name = name,
+                Price = price,
+                Quantity = quantity
+            };
+            inventory.AddProduct(product);
+        }
+    }
+}
+
